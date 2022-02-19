d4e9f2b6dd005ff5a0fbb8f148f24c882ddd0767
SadPhantom1 <logandang100@yahoo.com>
2022-02-19 11:15:10 -0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,38 @@
+'''
+Given a string s, find the length of the longest substring without repeating characters.
+'''
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    '''
+    Strategy: Store all possible solutions, find the longest one
+    '''
+    bank = []
+    i = 0
+    for i in range(len(s)):
+        temp = []
+        while i < len(s) and s[i] not in temp:
+            temp.append(s[i])
+            i += 1
+        bank.append(temp)
+        
+    print(bank)
+    greatest = 0
+    for sub in bank:
+        if len(sub) > greatest:
+            greatest = len(sub)
+    return greatest
+
+def lengthOfLongestSubstringCORRECT(s):
+    start = max_length = 0 
+    usedChar = {} # Maps char : number
+    
+    for i in range(len(s)):
+        if s[i] in usedChar and start <= usedChar[s[i]]:
+            start = usedChar[s[i]] + 1
+        else:
+            max_length = max(max_length, i - start + 1)
+
+        usedChar[s[i]] = i
+
+    return max_length
