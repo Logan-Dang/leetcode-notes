e7acfd8af93372b1636b6fdda8d23a71e6420007
Logan-Dang <76602370+Logan-Dang@users.noreply.github.com>
2022-07-16 21:05:41 -0700

    Shorten 2 add two numbers

---
+++ 2/main.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not (l1 and l2):
+            return l1 or l2
+        head = current = ListNode()
+        place = 0
+        while l1 or l2:
+            num = place
+            if l1:
+                num += l1.val
+                l1 = l1.next
+            if l2:
+                num += l2.val
+                l2 = l2.next
+            val = num % 10
+            place = num // 10
+            current.next = ListNode(val)
+            current = current.next
+        if place > 0:
+            current.next = ListNode(place)
+        return head.next
+    
+    def addTwoNumbersCORRECT(self, l1, l2):
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
