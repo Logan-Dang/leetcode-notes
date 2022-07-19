37c5e5c81cfb666d9709e55404eca71fda972825
Logan-Dang <76602370+Logan-Dang@users.noreply.github.com>
2022-07-19 16:14:46 -0700

    Update logan-sol.py

--- 24-swap-nodes-in-pairs/logan-sol.py
+++ 24-swap-nodes-in-pairs/logan-sol.py
@@ -1,32 +1,16 @@
-from typing import Optional, List
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
-def swapPairs(head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next: 
-            return head
-        
-        current = head
-        next = current.next
-        
-        current.next = next.next
-        next.next = current
-        head = next
-        
-        prev = current
-        current = current.next
-        if not current:
-            return head
-        next = current.next
-        while next:
-            prev.next = next
-            current.next = next.next
-            next.next = current
-            prev = current
-            current = current.next
-            if not current:
-                return head
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = current = ListNode(next = head)
+        while current.next and current.next.next:
             next = current.next
-        return head
+            mext = current.next.next.next
+            current.next = current.next.next
+            current.next.next = next
+            next.next = mext
+            current = next
+        return dummy.next
